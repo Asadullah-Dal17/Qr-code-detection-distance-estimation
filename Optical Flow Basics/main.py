5329377bbcec27a845d1c02a74e00adff14f513c
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-10-02 17:26:19 +0500

    updating basic optical flow

--- Optical Flow Basics/main.py
+++ Optical Flow Basics/main.py
@@ -10,9 +10,16 @@ def selectPoint(event, x, y, flags, params):
         # print(point)
         condition = True
         old_points = np.array([[x, y]], dtype=np.float32)
+
+lk_params = dict(winSize=(20, 20),
+                 maxLevel=4,
+                 criteria=(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.01))
+  
+        
 cap = cv.VideoCapture(0)
 _, frame = cap.read()
 old_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
 cv.namedWindow('frame')
 cv.setMouseCallback("frame", selectPoint)
 
@@ -28,17 +35,20 @@ while True:
     ret, frame = cap.read()
     cv.imshow('old frame ', old_gray)
     gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    print(old_points.astype(int))
+    #print(old_points.astype(int))
     if condition is True:
         cv.circle(frame, point, 5, (155, 0, 255), -1)
         new_points, status, error = cv.calcOpticalFlowPyrLK(
             old_gray, gray_frame, old_points, None, **lk_params)
         old_points = new_points
         new_points=new_points.astype(int)
-
-        print(type(new_points))
+        #print(type(new_points))
         x, y = new_points.ravel()
-        print(x, y)
+        cv.line(frame, (x+2,y-2) ,(x+20, y-30), AiPhile.GREEN, 2, cv.LINE_AA)
+        AiPhile.textBGoutline(frame, f'Tracking Point', (x+20, y-30),scaling=0.5)
+        
+        #print(x, y)
+        
         cv.circle(frame, (x, y), 6, (0, 255, 255), 4)
     fps = frame_counter/(time.time()-starting_time)
     AiPhile.textBGoutline(frame, f'FPS: {round(fps,1)}', (30, 40))
